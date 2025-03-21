3cf6c8df6df67500b56c4f9798279094c54732ba
latercomer <latercomer@qq.com>
2025-03-21 09:10:26 +0800

    恢复部分信息

--- bsp/simulator/rtconfig.py
+++ bsp/simulator/rtconfig.py
@@ -3,35 +3,35 @@ import sys
 import SCons.Tool.MSCommon.vc
 
 # toolchains options
-ARCH='sim'
+ARCH = 'sim'
 ASAN = False
-#CROSS_TOOL='msvc' or 'gcc' or 'mingw'
+# CROSS_TOOL='msvc' or 'gcc' or 'mingw'
 #'msvc' and 'mingw' are both for windows
 # 'gcc' is for linux
 if sys.platform == 'win32':
-    CROSS_TOOL='msvc'
+    CROSS_TOOL = 'msvc'
 else:
-    CROSS_TOOL='gcc'
+    CROSS_TOOL = 'gcc'
     ASAN = True
 
 if os.getenv('RTT_CC'):
-	CROSS_TOOL = os.getenv('RTT_CC')
+    CROSS_TOOL = os.getenv('RTT_CC')
 
 # cross_tool provides the cross compiler
-# EXEC_PATH is the compiler execute path 
-if  CROSS_TOOL == 'gcc' or CROSS_TOOL == 'clang-analyze':
-    CPU       = 'posix'
-    PLATFORM  = 'gcc'
+# EXEC_PATH is the compiler execute path
+if CROSS_TOOL == 'gcc' or CROSS_TOOL == 'clang-analyze':
+    CPU = 'posix'
+    PLATFORM = 'gcc'
     EXEC_PATH = '/usr/bin'
 
-elif  CROSS_TOOL == 'mingw':
-    CPU       = 'win32'
-    PLATFORM  = 'mingw'
+elif CROSS_TOOL == 'mingw':
+    CPU = 'win32'
+    PLATFORM = 'mingw'
     EXEC_PATH = r'D:\Program Files\CodeBlocks\MinGW\bin'
 
-elif  CROSS_TOOL == 'msvc':
-    CPU       = 'win32'
-    PLATFORM  = 'cl'
+elif CROSS_TOOL == 'msvc':
+    CPU = 'win32'
+    PLATFORM = 'cl'
     EXEC_PATH = ''
     try:
         vc_version = ''
@@ -55,10 +55,10 @@ else:
     exit(1)
 
 if os.getenv('RTT_EXEC_PATH'):
-	EXEC_PATH = os.getenv('RTT_EXEC_PATH')
+    EXEC_PATH = os.getenv('RTT_EXEC_PATH')
 
 BUILD = 'debug'
-#BUILD = ''
+# BUILD = ''
 
 if PLATFORM == 'gcc':
     # toolchains
@@ -77,7 +77,7 @@ if PLATFORM == 'gcc':
     CFLAGS = DEVICE + ' -I/usr/include -w -D_REENTRANT -D_LINUX -DHAVE_SYS_SIGNALS'
     AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp'
     # LFLAGS = DEVICE + ' -Wl,-Map=rtthread-linux.map -pthread -T gcc.ld' # open this when build 32bit target
-    LFLAGS = DEVICE + ' -Wl,-Map=rtthread-linux.map -pthread -T gcc_elf64.ld' # open this when build 64bit target
+    LFLAGS = DEVICE + ' -Wl,-Map=rtthread-linux.map -pthread -T gcc_elf64.ld'  # open this when build 64bit target
 
     CPATH = ''
     LPATH = ''
@@ -95,18 +95,18 @@ if PLATFORM == 'gcc':
 elif PLATFORM == 'mingw':
     # toolchains
     PREFIX = ''
-    CC      = PREFIX + 'gcc'
-    CXX     = PREFIX + 'g++'
-    AS      = PREFIX + 'gcc'
-    AR      = PREFIX + 'ar'
-    LINK    = PREFIX + 'g++'
+    CC = PREFIX + 'gcc'
+    CXX = PREFIX + 'g++'
+    AS = PREFIX + 'gcc'
+    AR = PREFIX + 'ar'
+    LINK = PREFIX + 'g++'
     TARGET_EXT = 'exe'
     SIZE = PREFIX + 'size'
     OBJDUMP = PREFIX + 'objdump'
     OBJCPY = PREFIX + 'objcopy'
 
     DEVICE = ''
-    CFLAGS = DEVICE + ' -D_Win32 -DNO_OLDNAMES -fno-pic -fno-builtin -fno-exceptions -fno-omit-frame-pointer -ffunction-sections -fdata-sections'
+    CFLAGS = DEVICE + ' -m32 -D_Win32 -DNO_OLDNAMES -fno-pic -fno-builtin -fno-exceptions -fno-omit-frame-pointer -ffunction-sections -fdata-sections'
 
     AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp'
     LFLAGS = DEVICE + ' -static-libgcc  -Wl,--gc-sections,-Map=rtthread-win32.map -T mingw.ld '
